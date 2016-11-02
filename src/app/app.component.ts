831ea15517f535b00c71e6ad65dc6391c0e64ad3
luran <luran@luopan88.com>
2016-11-02 19:23:57 +0800

    sds

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+// import '../../public/css/styles.css';
+@Component({
+  selector: 'my-app',
+  template: '<h1>my first app</h1>'
+  // styleUrls: ['./app.component.css']
+})
+export class AppComponent { }
